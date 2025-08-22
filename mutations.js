89f8759fdda034b9356ae06bc556c0da83d1c10e
MarginalizedConstituency <54913456+AlitoGP@users.noreply.github.com>
2025-08-22 15:09:02 +0200

    Update mutations.js

--- mutations.js
+++ mutations.js
@@ -4,6 +4,7 @@ const mutations = [
         color: "#4169e1", // Royal blue
         chance: 0.1, // 10% chance
         multiplier: 1.5, // 50% bonus to CPS
+        priceMultiplier: 1.25, // 25% increase to price
         eventRequired: "Rain" // Only appears during Rain events
     },
     {
@@ -11,39 +12,45 @@ const mutations = [
         color: "#ff3f34",
         chance: 0.1, 
         multiplier: 1.75,
+        priceMultiplier: 1.5,
         eventRequired: "Heatwave" 
     },
     {
         name: "Wild",
         color: "#d1bd92",
         chance: 0.1, 
-        multiplier: 1.25
+        multiplier: 1.25,
+        priceMultiplier: 1,
 
     },
     {
         name: "Gold",
         color: "#ffd700",
         chance: 0.02, 
-        multiplier: 2 
+        multiplier: 2,
+        priceMultiplier: 1.5,
 
     },
     {
         name: "Diamond",
         color: "#b9f2ff",
         chance: 0.0075, 
-        multiplier: 3
+        multiplier: 3,
+        priceMultiplier: 2,
     },
     {
         name: "Ruby",
         color: "#9b1113",
         chance: 0.00375, 
-        multiplier: 5
+        multiplier: 5,
+        priceMultiplier: 3,
     },
     {
         name: "Twisted",
         color: "#d0deec",
         chance: 0.15, 
         multiplier: 2.5,
+        priceMultiplier: 1.25,
         eventRequired: "Tornado"
     },
     {
@@ -51,6 +58,7 @@ const mutations = [
         color: "#592b1f",
         chance: 0.1, 
         multiplier: 0.95,
+        priceMultiplier: 0.8,
         eventRequired: "Masterchef"
     },
     {
@@ -58,6 +66,7 @@ const mutations = [
         color: "#a69085",
         chance: 0.1, 
         multiplier: 1.4,
+        priceMultiplier: 1.2,
         eventRequired: "Masterchef"
     },
     {
@@ -65,6 +74,7 @@ const mutations = [
         color: "#990000",
         chance: 0.08, 
         multiplier: 3.5,
+        priceMultiplier: 2.5,
         eventRequired: "Blood Moon"
     },
     {
@@ -72,6 +82,7 @@ const mutations = [
         color: "#b9e8ea",
         chance: 0.1, 
         multiplier: 1.5,
+        priceMultiplier: 1.2,
         eventRequired: "Hailstorm"
     },
     {
@@ -79,11 +90,13 @@ const mutations = [
         color: "#b0bf1a",
         chance: 0.2, 
         multiplier: 1.7,
+        priceMultiplier: 1.35,
         eventRequired: "Fallout"
     }
 
     
 
 ];
+
 
 
