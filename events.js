30723b0382e2cdb924ad1b1c4b07044f77f20305
MarginalizedConstituency <54913456+AlitoGP@users.noreply.github.com>
2025-08-22 12:30:04 +0200

    Update events.js

--- events.js
+++ events.js
@@ -9,6 +9,15 @@ const events = [
         duration: 180 // 3 minutes in seconds
     },
     {
+        name: "Hailstorm",
+        description: "10% Icy chance",
+        image: "",
+        audio: "sfx/hs.ogg",
+        chance: 0.03, // 3% chance every 3 seconds
+        spawnInterval: 3,
+        duration: 180 // 3 minutes in seconds
+    },
+    {
         name: "Heatwave",
         description: "10% Overheated chance",
         image: "https://cdn-icons-png.flaticon.com/512/6468/6468088.png",
@@ -16,6 +25,33 @@ const events = [
         chance: 0.02, // 
         spawnInterval: 5,
         duration: 120
+    },
+    {
+        name: "Tornado",
+        description: "15% Twisted chance",
+        image: "",
+        audio: "sfx/tornado.ogg",
+        chance: 0.06, 
+        spawnInterval: 10,
+        duration: 30 
+    },
+    {
+        name: "Blood Moon",
+        description: "12% Horror chance",
+        image: "",
+        audio: "sfx/bm.ogg",
+        chance: 0.08, 
+        spawnInterval: 30,
+        duration: 120
+    },
+    {
+        name: "Masterchef",
+        description: "10% Sauteed and Burnt chance",
+        image: "",
+        audio: "sfx/masterchef.ogg",
+        chance: 0.01, 
+        spawnInterval: 7,
+        duration: 300
     }
 
 ];
