17dc7a8236413cf9679877bfbd1b5b9df9990916
MarginalizedConstituency <54913456+AlitoGP@users.noreply.github.com>
2025-08-22 10:41:15 +0200

    Update events.js

--- events.js
+++ events.js
@@ -5,6 +5,7 @@ const events = [
         image: "https://cdn-icons-png.flaticon.com/512/1146/1146860.png",
         audio: "sfx/rain.ogg",
         chance: 0.03, // 3% chance every 3 seconds
+        spawnInterval: 3,
         duration: 180 // 3 minutes in seconds
     },
     {
@@ -13,7 +14,9 @@ const events = [
         image: "https://cdn-icons-png.flaticon.com/512/6468/6468088.png",
         audio: "sfx/hw.ogg",
         chance: 0.02, // 
+        spawnInterval: 5,
         duration: 120
     }
 
 ];
+
