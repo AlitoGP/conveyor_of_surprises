118f5636ecaf80649cedaee680fcd552f2f5a33f
MarginalizedConstituency <54913456+AlitoGP@users.noreply.github.com>
2025-08-22 16:46:44 +0200

    Update items.js

--- items.js
+++ items.js
@@ -17,21 +17,21 @@ const items = [
         name: "Rabbit",
         image: "https://cdn-icons-png.flaticon.com/512/10806/10806489.png",
         chance: 15, // Rare rarity (20-99 range)
-        cps: 10,
+        cps: 7,
         price: 180
     },
     {
         name: "Bear",
         image: "https://cdn-icons-png.flaticon.com/512/2938/2938228.png",
         chance: 6, 
-        cps: 50,
+        cps: 35,
         price: 1250
     },
     {
         name: "Dragon",
         image: "https://cdn-icons-png.flaticon.com/512/5169/5169269.png",
         chance: 1, 
-        cps: 500,
+        cps: 250,
         price: 18000
     }
 
